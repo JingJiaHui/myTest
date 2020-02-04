eea3037d3d0adec5c1d69e0627648c5958c086c1
jjh <18309895679@163.com>
2020-02-04 14:41:16 +0800

    java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将打印字符串 Hello World
+     */
+    public static void main(String []args) {
+        System.out.println("Hello World"); // 打印 Hello World
+		 System.out.println("Hello 1"); // 打印 Hello World
+		  System.out.println("Hello 2"); // 打印 Hello World
+		   System.out.println("Hello 3"); // 打印 Hello World
+    }
+}
